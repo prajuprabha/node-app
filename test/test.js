3c79457b0da33801ba60468774ddebed44e036b1
Yosua Tresno Surojo <yosua1011@gmail.com>
2018-09-28 16:13:42 +0700

    simple node app with test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../index.js');
 
 describe('GET /', function() {
-    it('respond with hello world', function(done) {
+    it('respond with hello awesome', function(done) {
         //navigate to root and check the response is "hello world"
         request(app).get('/').expect('hello awesome', done);
     });
