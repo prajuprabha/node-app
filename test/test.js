07b5e9b3b016711d49b95ad512482dc203f551af
Anji Reddy <33461522+Anji4546@users.noreply.github.com>
2019-04-03 12:53:54 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('node-app', () => {
     })
 
     describe('GET /', function() {
-        it('respond with hello anjireddy', (done) => {
+        it('respond with Welcome to Jenkins', (done) => {
             //navigate to root and check the response is "hello world"
             request(app).get('/').expect('hello anjireddy').end(done);
         });
