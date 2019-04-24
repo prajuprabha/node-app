720cc8a0a58dd0625875266ed89fe23f817f5003
prajuprabha <49267518+prajuprabha@users.noreply.github.com>
2019-04-24 17:09:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Piedistrict Private Limited Middle East, Europe and United States!.');
+ res.send('Piedistrict Private Limited Middle East, Europe and United States.');
 });
 //listen to port 3000 by default
 var port = process.env.PORT || 3000
